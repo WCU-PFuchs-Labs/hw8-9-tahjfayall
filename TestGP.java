a25ffc950289af39408ce0ac5c6827682995ab10
tahjfayall <3019669@wcupa.edu>
2025-11-05 02:03:22 -0500

    Add files via upload

---
+++ TestGP.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class TestGP
+{
+   public static void main (String [] args)
+   {
+      Scanner kb = new Scanner(System.in);
+      
+      System.out.println("Enter file name: ");
+      String fileName = kb.nextLine();
+      
+      Generation gen = new Generation (500, 3, fileName);
+      
+      gen.evalAll();
+      gen.printBestTree();
+      gen.printBestFitness();
+      
+      ArrayList<GPTree> topTen = gen.getTopTen();
+      System.out.print("Top Ten fitness values: ");
+      for (int i = 0; i < topTen.size(); i++)
+      {
+         System.out.printf("%.2f", topTen.get(i).getFitness());
+         if (i < topTen.size() -1)
+         {
+            System.out.print(", ");
+         }
+      }
+      System.out.println();
+   }
+}
